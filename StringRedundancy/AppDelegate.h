15ce1f3fb9470d481f654c35f2bc2dbf6a7e23ec
Rakesh <082rakesh@gmail.com>
2015-09-15 16:16:54 +0530

    Initial Commit

---
+++ StringRedundancy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StringRedundancy
+//
+//  Created by Ranjna Mishra on 15/09/15.
+//  Copyright (c) 2015 Ranjna Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StringRedundancy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StringRedundancy
+//
+//  Created by Ranjna Mishra on 15/09/15.
+//  Copyright (c) 2015 Ranjna Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
